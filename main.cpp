2836adfbd68f97ba494324e82d0d5860e77d3b42
abd <abd.manian@gmail.com>
2022-01-23 23:19:04 +0330

    Initial import for function based bouncing ball

--- main.cpp
+++ main.cpp
@@ -13,6 +13,44 @@ struct Ball
 	Color color;
 };
 
+void move_ball(Ball& ball)
+{
+	ball.x = ball.x + ball.speed_x;
+	ball.y = ball.y + ball.speed_y;
+}
+
+void bounce_ball(Ball& ball, int width, int height)
+{
+	if (ball.y>(height-ball.radius))
+	{
+		ball.y = height - ball.radius;
+		ball.speed_y = ball.speed_y * -1 ;
+	}
+
+	if (ball.x>(width-ball.radius))
+	{
+		ball.x = width - ball.radius;
+		ball.speed_x = ball.speed_x * -1;
+	}
+
+	if (ball.y<ball.radius)
+	{
+		ball.y = ball.radius;
+		ball.speed_y = ball.speed_y * -1;
+	}
+
+	if (ball.x<ball.radius)
+	{
+		ball.x = ball.radius;
+		ball.speed_x = ball.speed_x * -1;
+	}
+
+}
+
+void draw_ball(Ball ball)
+{
+    DrawCircle(ball.x, ball.y, ball.radius, ball.color);
+}
 
 int main(void)
 {
@@ -23,8 +61,9 @@ int main(void)
     const int start_x = screen_width / 3;
     const int start_y = screen_height / 3;
 
-    Ball ball_r {start_x, start_y, 10.0f, -4, -4, RED };
-    Ball ball_b {start_x, start_y, 15.0f, -4,  4, BLUE};
+    Ball ball_r {start_x, start_y,  8.0f, -4, -4, RED  };
+    Ball ball_b {start_x, start_y, 12.0f, -4,  4, BLUE };
+    Ball ball_g {start_x, start_y, 16.0f,  4,  4, GREEN};
 
     InitWindow(screen_width, screen_height, "Bouncing Ball!");
 
@@ -34,61 +73,13 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+    	move_ball(ball_r);
+    	move_ball(ball_b);
+    	move_ball(ball_g);
 
-    	ball_r.x = ball_r.x + ball_r.speed_x;
-    	ball_r.y = ball_r.y + ball_r.speed_y;
-
-    	if (ball_r.y>(screen_height-ball_r.radius))
-    	{
-    		ball_r.y = screen_height - ball_r.radius;
-    		ball_r.speed_y = ball_r.speed_y * -1 ;
-    	}
-
-    	if (ball_r.x>(screen_width-ball_r.radius))
-    	{
-    		ball_r.x = screen_width - ball_r.radius;
-    		ball_r.speed_x = ball_r.speed_x * -1;
-    	}
-
-    	if (ball_r.y<ball_r.radius)
-    	{
-    		ball_r.y = ball_r.radius;
-    		ball_r.speed_y = ball_r.speed_y * -1;
-    	}
-
-    	if (ball_r.x<ball_r.radius)
-    	{
-    		ball_r.x = ball_r.radius;
-    		ball_r.speed_x = ball_r.speed_x * -1;
-    	}
-
-
-    	ball_b.x = ball_b.x + ball_b.speed_x;
-    	ball_b.y = ball_b.y + ball_b.speed_y;
-
-    	if (ball_b.y>(screen_height-ball_b.radius))
-    	{
-    		ball_b.y = screen_height - ball_b.radius;
-    		ball_b.speed_y = ball_b.speed_y * -1 ;
-    	}
-
-    	if (ball_b.x>(screen_width-ball_b.radius))
-    	{
-    		ball_b.x = screen_width - ball_b.radius;
-    		ball_b.speed_x = ball_b.speed_x * -1;
-    	}
-
-    	if (ball_b.y<ball_b.radius)
-    	{
-    		ball_b.y = ball_b.radius;
-    		ball_b.speed_y = ball_b.speed_y * -1;
-    	}
-
-    	if (ball_b.x<ball_b.radius)
-    	{
-    		ball_b.x = ball_b.radius;
-    		ball_b.speed_x = ball_b.speed_x * -1;
-    	}
+    	bounce_ball(ball_r, screen_width, screen_height);
+    	bounce_ball(ball_b, screen_width, screen_height);
+    	bounce_ball(ball_g, screen_width, screen_height);
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -96,8 +87,9 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawCircle(ball_r.x, ball_r.y, ball_r.radius, ball_r.color);
-            DrawCircle(ball_b.x, ball_b.y, ball_b.radius, ball_b.color);
+            draw_ball(ball_r);
+            draw_ball(ball_b);
+            draw_ball(ball_g);
 
             DrawFPS(10, 10);
 
